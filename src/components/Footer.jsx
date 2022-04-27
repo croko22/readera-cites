2c29a6f182d860b7239117975912f9eb90668e25
Kevin <kchambit@unsa.edu.pe>
2022-04-27 15:15:09 -0500

    Micro Update 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="bg-dark text-center text-lg-start text-white mt-auto">
         <div className="container p-2">
         <div className="inner text-center">
-          <p>Cites exractor for <a href="https://readera.org/es/book-reader" target="_blank"rel="noopener noreferrer">Readera</a>, by <a href="https://twitter.com/croko22"target="_blank"rel="noopener noreferrer">@croko22</a>.</p>
+          <p>Cites exractor for <a href="https://readera.org/es/book-reader" target="_blank"rel="noopener noreferrer">Readera</a>, by <a href="https://github.com/Croko22"target="_blank"rel="noopener noreferrer">@croko22</a>.</p>
         </div>
         </div>
     </footer>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: '/docs/index.html'
-  }
+    open: '/dist/index.html'
+  },
 })
