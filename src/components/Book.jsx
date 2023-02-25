18bae4dc1dc8dcede56b5ce474588d17c36429e4
Kevin Chambi <kchambit@unsa.edu.pe>
2023-02-25 12:15:50 -0500

    Show book progress

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,55 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import Toastify from 'toastify-js'
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Toastify from "toastify-js";
+import { useLocation } from "react-router-dom";
 import { FaCopy } from "react-icons/fa";
-import { CitesList } from './CitesList';
-import { Search } from './Search';
+import { CitesList } from "./CitesList";
+import { Search } from "./Search";
 
 export const Book = () => {
-  //Book data extracting
+  //*Book data extracting
   const Book = useLocation().state;
-  const [Cites,setCites] = useState(Book.citations)
-  //Scroll to the top 
-  useEffect(() => {window.scrollTo(0, 0);}, []); 
+  const [Cites, setCites] = useState(Book.citations);
+  //*Scroll to the top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-  //Filtering and searching
-  const [searchText, setSearchText] = useState("")
-  const filterColors=(n)=>{
-    (n===7) ? setCites(Book.citations) : setCites(Book.citations.filter(cite=>cite.note_mark===n))
-  }
+  //*Filtering and searching
+  const [searchText, setSearchText] = useState("");
+  const filterColors = (n) => {
+    n === 7
+      ? setCites(Book.citations)
+      : setCites(Book.citations.filter((cite) => cite.note_mark === n));
+  };
 
-  const copyCites = ()=>{
-    let text=''
-    for(const a in Cites) text+=`- ${Cites[a].note_body}\n`
+  const copyCites = () => {
+    let text = "";
+    for (const a in Cites) text += `- ${Cites[a].note_body}\n`;
     navigator.clipboard.writeText(text);
     Toastify({
-      text: "All quotes copied to clipboard", 
+      text: "All quotes copied to clipboard",
       close: true,
-      gravity: "top", 
-      position: "left", 
-      stopOnFocus: true, 
-      duration: 3000
+      gravity: "top",
+      position: "left",
+      stopOnFocus: true,
+      duration: 3000,
     }).showToast();
   };
 
   return (
     <div className="container">
       <h3>Book: {Book.data.doc_title}</h3>
-      <Search handleSearchNote={setSearchText}/>
+      <Search handleSearchNote={setSearchText} />
       {/* Filtering by color */}
       <div className="mt-1 d-flex justify-content-between">
-        <span className="h4">Filter by color: 
-        <span onClick={() => filterColors(7)} className="btn btn-info m-1">All {Book.citations.length}</span>
-        <span onClick={() => filterColors(0)} className="btn btn-secondary m-1">{Book.citations.filter(cite=>cite.note_mark===0).length}</span>
-        <span onClick={() => filterColors(1)} className="btn btn-danger m-1">{Book.citations.filter(cite=>cite.note_mark===1).length}</span>
-        <span onClick={() => filterColors(2)} className="btn btn-warning m-1">{Book.citations.filter(cite=>cite.note_mark===2).length}</span>
-        <span onClick={() => filterColors(3)} className="btn btn-success m-1">{Book.citations.filter(cite=>cite.note_mark===3).length}</span>
-        <span onClick={() => filterColors(4)} className="btn btn-primary m-1">{Book.citations.filter(cite=>cite.note_mark===4).length}</span>
+        <span className="h4">
+          Filter by color:
+          <span onClick={() => filterColors(7)} className="btn btn-info m-1">
+            All {Book.citations.length}
+          </span>
+          <span
+            onClick={() => filterColors(0)}
+            className="btn btn-secondary m-1"
+          >
+            {Book.citations.filter((cite) => cite.note_mark === 0).length}
+          </span>
+          <span onClick={() => filterColors(1)} className="btn btn-danger m-1">
+            {Book.citations.filter((cite) => cite.note_mark === 1).length}
+          </span>
+          <span onClick={() => filterColors(2)} className="btn btn-warning m-1">
+            {Book.citations.filter((cite) => cite.note_mark === 2).length}
+          </span>
+          <span onClick={() => filterColors(3)} className="btn btn-success m-1">
+            {Book.citations.filter((cite) => cite.note_mark === 3).length}
+          </span>
+          <span onClick={() => filterColors(4)} className="btn btn-primary m-1">
+            {Book.citations.filter((cite) => cite.note_mark === 4).length}
+          </span>
         </span>
         {/* Export quotes button */}
-        <div className='btn btn-dark m-1' onClick={() => copyCites()}><FaCopy/> Copy all quotes to the clipboard</div>
+        <div className="btn btn-dark m-1" onClick={() => copyCites()}>
+          <FaCopy /> Copy all quotes to the clipboard
+        </div>
       </div>
-      <CitesList cites={Cites.filter((cite)=> cite.note_body.toLowerCase().includes(searchText))}/>
+      <CitesList
+        cites={Cites.filter((cite) =>
+          cite.note_body.toLowerCase().includes(searchText)
+        )}
+      />
     </div>
-  )
-}
+  );
+};
--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-export const BookCard = ({libro}) => {
+import React from "react";
+import { Accordion, ProgressBar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+export const BookCard = ({ libro, bookKey }) => {
   return (
-    <Link to="/book" state={libro} className="list-group-item list-group-item-action flex-column align-items-start">
-        <div className="d-flex w-100 justify-content-between">
-        <h5 className="mb-1">{libro.data.doc_file_name_title} {libro.data.doc_favorites_time!==0 && <span className="btn btn-warning">Fav</span>}</h5>
+    <Accordion.Item eventKey={bookKey}>
+      <Accordion.Header>
+        <h5 className="mb-1">
+          {libro.data.doc_file_name_title}{" "}
+          {libro.data.doc_favorites_time !== 0 && (
+            <span className="btn btn-warning">Fav</span>
+          )}
+        </h5>
         <small>{libro.citations.length} quotes</small>
-        </div>
-    </Link>
-  )
-}
+      </Accordion.Header>
+
+      <Accordion.Body>
+        <p className="mb-1">
+          <b>Author:</b> {libro.data.doc_authors}
+        </p>
+        <ProgressBar
+          now={Math.round(JSON.parse(libro.data.doc_position).ratio * 100)}
+          label={`Progress: ${Math.round(
+            JSON.parse(libro.data.doc_position).ratio * 100
+          )}%`}
+        />
+        <Link
+          to="/book"
+          state={libro}
+          className="mt-2 list-group-item list-group-item-action flex-column align-items-start"
+        >
+          Go to book
+        </Link>
+      </Accordion.Body>
+    </Accordion.Item>
+  );
+};
