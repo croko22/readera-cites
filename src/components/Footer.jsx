6477c448fc010c4f7c875f57a79ff9569c9a522c
Kevin Chambi <kchambit@unsa.edu.pe>
2023-03-02 11:02:12 -0500

    Footer micro update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
   return (
     <footer className="bg-dark text-center text-lg-start text-white mt-auto">
       <div className="container p-2">
         <div className="inner text-center">
-          <p>Book notes visualizer for <a href="https://readera.org/es/book-reader" target="_blank"rel="noopener noreferrer">Readera</a>, by <a href="https://github.com/kchambit" target="_blank" rel="noopener noreferrer">Kevin Chambi</a>.</p>
+          <p>
+            Book notes visualizer for{" "}
+            <a
+              href="https://readera.org/es/book-reader"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Readera
+            </a>
+            , by{" "}
+            <a
+              href="https://github.com/kchambit"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CrokoDev
+            </a>
+            .
+          </p>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
