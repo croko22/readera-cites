38c0364ed2616f3759c00e56d1754c2b1a8af240
kchambit <kev132011@hotmail.com>
2023-04-11 17:10:33 -0500

    Finally fixed the carousel card

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,46 +1,62 @@
 import React from "react";
-import { Accordion, ProgressBar } from "react-bootstrap";
+import {
+  Accordion,
+  Card,
+  ProgressBar,
+  useAccordionButton,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { RiArrowDropDownLine } from "react-icons/ri";
+
 export const BookCard = ({ libro, bookKey }) => {
   return (
-    <Accordion.Item eventKey={bookKey}>
-      <Accordion.Header as="div">
-        <h5>{libro.data.doc_file_name_title}</h5>
-        <span>
-          {libro.data.doc_favorites_time !== 0 && (
-            <span className="btn btn-warning">Fav</span>
-          )}
-          <small>{libro.citations.length} quotes</small>
-        </span>
-      </Accordion.Header>
+    <Card eventKey={bookKey}>
+      <Card.Header as="div">
+        <div className="d-flex w-100 justify-content-between">
+          <Link
+            to={`/book/${libro.data.doc_md5}`}
+            state={libro}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <h5 className="mb-1">
+              {libro.data.doc_file_name_title}{" "}
+              {libro.data.doc_favorites_time !== 0 && (
+                <span className="btn btn-warning">Fav</span>
+              )}
+            </h5>
+          </Link>
+          <span>
+            <small>{libro.citations.length} quotes</small>
+            <RiArrowDropDownLine
+              className="h2"
+              onClick={useAccordionButton(bookKey)}
+            ></RiArrowDropDownLine>
+          </span>
+        </div>
+      </Card.Header>
 
-      <Accordion.Body>
-        <p className="mb-1 d-flex justify-content-between">
-          <span>
-            <b>Author:</b> {libro.data.doc_authors}
-          </span>
-          <span>
-            Rating:{" "}
-            {libro.data.doc_rating > 0
-              ? "⭐".repeat(libro.data.doc_rating)
-              : "None"}
-          </span>
-          <span></span>
-        </p>
-        <ProgressBar
-          now={Math.round(JSON.parse(libro.data.doc_position).ratio * 100)}
-          label={`Progress: ${Math.round(
-            JSON.parse(libro.data.doc_position).ratio * 100
-          )}%`}
-        />
-        <Link
-          to={`/book/${libro.data.doc_md5}`}
-          state={libro}
-          className="mt-2 list-group-item list-group-item-action flex-column align-items-start"
-        >
-          Go to book
-        </Link>
-      </Accordion.Body>
-    </Accordion.Item>
+      <Accordion.Collapse eventKey={bookKey}>
+        <Card.Body>
+          <p className="mb-1 d-flex justify-content-between">
+            <span>
+              <b>Author:</b> {libro.data.doc_authors}
+            </span>
+            <span>
+              Rating:{" "}
+              {libro.data.doc_rating > 0
+                ? "⭐".repeat(libro.data.doc_rating)
+                : "None"}
+            </span>
+            <span></span>
+          </p>
+          <ProgressBar
+            now={Math.round(JSON.parse(libro.data.doc_position).ratio * 100)}
+            label={`Progress: ${Math.round(
+              JSON.parse(libro.data.doc_position).ratio * 100
+            )}%`}
+          />
+        </Card.Body>
+      </Accordion.Collapse>
+    </Card>
   );
 };
