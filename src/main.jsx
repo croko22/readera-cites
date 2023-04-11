ee29ac20da728cb7b65d80e49048370270aaee92
kchambit <kev132011@hotmail.com>
2023-04-11 17:10:33 -0500

    PWA attemp and sitemap

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import App from "./App";
 import bootstrap from "bootstrap/dist/css/bootstrap.css";
 import "toastify-js/src/toastify.css";
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "ReadEra BookNotes",
+        short_name: "ReadEra BookNotes",
+        description: "ReadEra BookNotes",
+        icons: [
+          {
+            src: "android/android-launchericon-512-512.png",
+            sizes: "512x512",
+            purpose: "any maskable",
+          },
+          {
+            src: "android/android-launchericon-192-192.png",
+            sizes: "192x192",
+          },
+          {
+            src: "android/android-launchericon-144-144.png",
+            sizes: "144x144",
+          },
+          {
+            src: "android/android-launchericon-96-96.png",
+            sizes: "96x96",
+          },
+          {
+            src: "android/android-launchericon-72-72.png",
+            sizes: "72x72",
+          },
+          {
+            src: "android/android-launchericon-48-48.png",
+            sizes: "48x48",
+          },
+        ],
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
   server: {
-    open: '/'
-  }
-})
+    open: "/",
+  },
+});
