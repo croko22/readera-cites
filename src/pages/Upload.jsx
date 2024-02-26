1fa47c94cf6cfd7e20880bf411e6513867b7b526
croko22 <kvchambit@gmail.com>
2024-02-26 11:19:21 -0500

    Handle no file uploaded exception

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -18,13 +18,20 @@ export const Upload = () => {
   };
 
   const setLocalStorage = () => {
+    if (!files) {
+      Toastify({
+        text: "No file selected!",
+        position: "center",
+        duration: 1500,
+        style: { background: "linear-gradient(to right, #ff416c, #ff4b2b)" },
+      }).showToast();
+      return;
+    }
     try {
       window.localStorage.setItem("Books", files);
-      //* Toast of success
       navigate("/");
       Toastify({
         text: "Loaded succesfully!",
-        position: "top-left",
         duration: 1500,
         style: { background: "linear-gradient(to right, #00b09b, #96c93d)" },
       }).showToast();
@@ -34,39 +41,51 @@ export const Upload = () => {
   };
 
   return (
-    <div className="container row d-flex justify-content-center align-items-center mx-auto">
-      {/* <div className="col-sm"></div> */}
-      <div className="col-sm py-3">
-        <h1>Hi</h1>
-        <p>
-          In the ReadEra app, go to the Backup and restore section and export a
-          security copy ReadEra backup file.
-        </p>
-        <img src="assets/img/bak file.jpg" alt="upload" className="img-fluid" />
-        <p>
-          This will create a .bak file, inside which is a file called
-          library.json that contains your books data.
-        </p>
-        <img src="assets/img/json file.png" alt="json" className="img-fluid" />
-      </div>
-      <div className="col-sm">
-        <h1 className="text-center mb-4">Upload Json File</h1>
-        <p className="mb-3">
-          Visualize and manage your readings data from <b>library.json</b> file
-          inside your ReadEra .bak file
-        </p>
-        <input
-          className="form-control form-control-lg mb-3"
-          type="file"
-          accept=".json"
-          onChange={handleChange}
-        />
-        <button
-          className="btn btn-lg btn-success mt-3 "
-          onClick={setLocalStorage}
-        >
-          Save library
-        </button>
+    <div className="container mt-3">
+      <div className="row gap-3 align-items-center">
+        <div className="col-sm">
+          <h1>Welcome to ReadEra - Book Notes</h1>
+          <p>
+            In the ReadEra app, navigate to the <b>Backup & Restore</b> section
+            and export a ReadEra backup file.
+          </p>
+          <img
+            src="assets/img/bak-file.jpg"
+            alt="upload"
+            className="img-fluid"
+          />
+          <p className="py-3">
+            This will create a <b>.bak</b> file containing a{" "}
+            <code>library.json</code> file that holds your ReadEra data.
+          </p>
+          <img
+            src="assets/img/json-file.png"
+            alt="json"
+            className="img-fluid"
+          />
+        </div>
+        <div className="col-sm">
+          <h2 className="mt-2 mb-4">
+            Upload your <code>library.json</code> file
+          </h2>
+          <p className="mb-3">
+            Visualize and manage your reading data from the{" "}
+            <code>library.json</code> file inside your ReadEra <code>.bak</code>{" "}
+            file.
+          </p>
+          <input
+            className="form-control form-control-lg mb-3"
+            type="file"
+            accept=".json"
+            onChange={handleChange}
+          />
+          <button
+            className="btn btn-lg btn-success mt-3"
+            onClick={setLocalStorage}
+          >
+            Save Library
+          </button>
+        </div>
       </div>
     </div>
   );
