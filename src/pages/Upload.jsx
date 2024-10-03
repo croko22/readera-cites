b383d75a61061180dd607b5daecea750d70865ce
croko22 <kvchambit@gmail.com>
2024-10-03 07:43:08 -0500

    feat: Add dynamic minimum number of citations in Upload page

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -5,17 +5,19 @@ import Toastify from "toastify-js";
 
 export const Upload = () => {
   const [files, setFiles] = useState();
+  const [books, setBooks] = useState();
+  const [minCitations, setMinCitations] = useState(100); // New state for minimum citations
   const navigate = useNavigate();
 
-  //TODO: Migrate to indexedDB
   const handleChange = (e) => {
     const fileReader = new FileReader();
     fileReader.readAsText(e.target.files[0], "UTF-8");
     fileReader.onload = (e) => {
       const data = JSON.parse(e.target.result).docs.filter(
-        (book) => book.citations.length > 100
+        (book) => book.citations.length > minCitations // Use the dynamic value
       );
       setFiles(JSON.stringify(data));
+      setBooks(data);
     };
   };
 
@@ -33,7 +35,7 @@ export const Upload = () => {
       window.localStorage.setItem("Books", files);
       navigate("/");
       Toastify({
-        text: "Loaded succesfully!",
+        text: "Loaded successfully!",
         duration: 1500,
         style: { background: "linear-gradient(to right, #00b09b, #96c93d)" },
       }).showToast();
@@ -75,6 +77,18 @@ export const Upload = () => {
             <code>library.json</code> file inside your ReadEra <code>.bak</code>{" "}
             file.
           </p>
+          <div className="mb-3">
+            <label htmlFor="citations" className="form-label">
+              Minimum number of citations:
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="citations"
+              value={minCitations}
+              onChange={(e) => setMinCitations(Number(e.target.value))} // Update state on change
+            />
+          </div>
           <div className="d-flex gap-1 align-items-center">
             <input
               className="form-control form-control-lg"
@@ -89,6 +103,15 @@ export const Upload = () => {
             >
               <FaUpload className="font-sm" />
             </button>
+
+            {files && (
+              <div>
+                <span className="badge bg-success rounded-pill">
+                  File loaded
+                </span>
+                {books && books.length} books
+              </div>
+            )}
           </div>
         </div>
       </div>
